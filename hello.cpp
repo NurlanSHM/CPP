2cb6e37354950935cb95b19e82b0996290b63055
NurlanSHM <flyxcookie@gmail.com>
2025-04-22 16:44:01 +0400

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    
+    //This is a comment
+    /*
+    this is
+    a 
+    multi-line commennt
+    */
+    std::cout << "u sure this aint ASM" << '\n';
+    std::cout << "faster than python u said" << '\n';
+    return 0;
+}
---
+++ var-basicdata.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(){
+
+    //integer
+    int age = 14;
+    int year = 2025;
+    int days = 7.5; // decimal, int wont take it, only 7.
+
+    // double (basically python float)
+    double price = 10.99;
+    double gpa = 4.98;
+    double temp = 36.6;
+
+    // std::cout << days;
+    std :: cout << price;
+
+    //single character
+    char grade = 'A';
+    char Bravo = 'B';
+
+    std :: cout << Bravo;
+
+    //boolean 
+    bool student = true;
+    bool power_off = false;
+
+    return 0;
+}
+
+
+
+
+//    std::cout << x << '\n';
+//    std::cout << y << '\n';
+//    std::cout << "the sum is: " << sum;
